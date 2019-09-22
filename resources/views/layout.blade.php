f80f39dd145556196a4989d71587fbe12dc7eab8
Cesar Rodriguez <crcesar15@gmail.com>
2019-09-22 12:21:22 -0400

    AdmLte added to the project and it's working with blade

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+        @yield('navigation')
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <h1>
+                    Dashboard
+                    <small>Version 2.0</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                    <li class="active">Dashboard</li>
+                </ol>
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+
+            </section>
+        </div>
+        <!-- /.content-wrapper -->
+        @yield('footer')
+    </div>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+
+</html>
